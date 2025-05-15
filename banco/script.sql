b62c04c39d877109f26a130910316e8c70c75c15
Paloma de Castro Leite <94177979+palomac1@users.noreply.github.com>
2025-05-15 17:53:15 -0300

    att script

--- banco/script.sql
+++ banco/script.sql
@@ -18,34 +18,6 @@ CREATE SCHEMA IF NOT EXISTS `db_ebenezer` DEFAULT CHARACTER SET utf8mb4 ;
 USE `db_ebenezer` ;
 
 -- -----------------------------------------------------
--- Table `db_ebenezer`.`voluntarios`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_ebenezer`.`voluntarios` (
-  `cod_voluntario` INT(11) NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(60) NOT NULL,
-  `cpf` VARCHAR(11) NOT NULL,
-  `data_nasc` DATETIME NOT NULL,
-  `email` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`cod_voluntario`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 5
-DEFAULT CHARACTER SET = utf8mb4;
-
-
--- -----------------------------------------------------
--- Table `db_ebenezer`.`ong`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_ebenezer`.`ong` (
-  `cod_ong` INT(11) NOT NULL AUTO_INCREMENT,
-  `CNPJ` INT(11) NOT NULL,
-  `descricao` VARCHAR(1000) NOT NULL,
-  PRIMARY KEY (`cod_ong`))
-ENGINE = InnoDB
-AUTO_INCREMENT = 12
-DEFAULT CHARACTER SET = utf8mb4;
-
-
--- -----------------------------------------------------
 -- Table `db_ebenezer`.`endereco`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db_ebenezer`.`endereco` (
@@ -57,28 +29,7 @@ CREATE TABLE IF NOT EXISTS `db_ebenezer`.`endereco` (
   `bairro` VARCHAR(60) NOT NULL,
   `cidade` VARCHAR(100) NOT NULL,
   `uf` CHAR(2) NOT NULL,
-  `cod_voluntario` INT(11) NOT NULL,
-  `cod_ong` INT(11) NOT NULL,
-  `cod_filial` INT(11) NOT NULL,
-  PRIMARY KEY (`cod_endereco`),
-  INDEX `fk_endereco_cl_voluntarios1_idx` (`cod_voluntario` ASC) VISIBLE,
-  INDEX `fk_endereco_ong_idx` (`cod_ong` ASC) VISIBLE,
-  INDEX `fk_endereco_filial_idx` (`cod_filial` ASC) VISIBLE,
-  CONSTRAINT `fk_endereco_cl_voluntarios1`
-    FOREIGN KEY (`cod_voluntario`)
-    REFERENCES `db_ebenezer`.`voluntarios` (`cod_voluntario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_endereco_ong`
-    FOREIGN KEY (`cod_ong`)
-    REFERENCES `db_ebenezer`.`ong` (`cod_ong`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_endereco_filial`
-    FOREIGN KEY (`cod_filial`)
-    REFERENCES `db_ebenezer`.`filial` (`cod_filial`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`cod_endereco`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
@@ -90,28 +41,7 @@ CREATE TABLE IF NOT EXISTS `db_ebenezer`.`telefone` (
   `cod_telefone` INT(11) NOT NULL AUTO_INCREMENT,
   `numero` VARCHAR(9) NOT NULL,
   `ddd` VARCHAR(2) NOT NULL,
-  `cod_voluntario` INT(11) NOT NULL,
-  `cod_filial` INT(11) NOT NULL,
-  `cod_ong` INT(11) NOT NULL,
-  PRIMARY KEY (`cod_telefone`),
-  INDEX `fk_telefone_voluntarios1_idx` (`cod_voluntario` ASC) VISIBLE,
-  INDEX `fk_telefone_filial_idx` (`cod_filial` ASC) VISIBLE,
-  INDEX `fk_telefone_ong_idx` (`cod_ong` ASC) VISIBLE,
-  CONSTRAINT `fk_telefone_voluntarios1`
-    FOREIGN KEY (`cod_voluntario`)
-    REFERENCES `db_ebenezer`.`voluntarios` (`cod_voluntario`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_telefone_filial`
-    FOREIGN KEY (`cod_filial`)
-    REFERENCES `db_ebenezer`.`filial` (`cod_filial`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_telefone_ong`
-    FOREIGN KEY (`cod_ong`)
-    REFERENCES `db_ebenezer`.`ong` (`cod_ong`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`cod_telefone`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
@@ -121,20 +51,19 @@ DEFAULT CHARACTER SET = utf8mb4;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db_ebenezer`.`filial` (
   `cod_filial` INT(11) NOT NULL AUTO_INCREMENT,
+  `cod_endereco` INT(11) NOT NULL,
+  `cod_telefone` INT(11) NOT NULL,
   PRIMARY KEY (`cod_filial`),
-  CONSTRAINT `fk_cod_endereco`
-    FOREIGN KEY ()
-    REFERENCES `db_ebenezer`.`endereco` ()
+  INDEX `cod_endereco_idx` (`cod_endereco` ASC) VISIBLE,
+  INDEX `cod_telefone_idx` (`cod_telefone` ASC) VISIBLE,
+  CONSTRAINT `cod_endereco`
+    FOREIGN KEY (`cod_endereco`)
+    REFERENCES `db_ebenezer`.`endereco` (`cod_endereco`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT ``
-    FOREIGN KEY ()
-    REFERENCES `db_ebenezer`.`telefone` ()
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_cod_filial`
-    FOREIGN KEY ()
-    REFERENCES `db_ebenezer`.`filial` ()
+  CONSTRAINT `cod_telefone`
+    FOREIGN KEY (`cod_telefone`)
+    REFERENCES `db_ebenezer`.`telefone` (`cod_telefone`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB
@@ -164,6 +93,71 @@ DEFAULT CHARACTER SET = utf8mb4;
 
 
 -- -----------------------------------------------------
+-- Table `db_ebenezer`.`doacoes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_ebenezer`.`doacoes` (
+  `cod_doacoes` INT(11) NOT NULL,
+  `nome_doador` VARCHAR(45) NOT NULL,
+  `data` DATE NOT NULL,
+  PRIMARY KEY (`cod_doacoes`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `db_ebenezer`.`dinheiro`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_ebenezer`.`dinheiro` (
+  `valor` FLOAT NOT NULL,
+  `cod_doacao` INT(11) NOT NULL,
+  INDEX `cod_doacao` (`cod_doacao` ASC) VISIBLE,
+  CONSTRAINT `cod_doacao`
+    FOREIGN KEY (`cod_doacao`)
+    REFERENCES `db_ebenezer`.`doacoes` (`cod_doacoes`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `db_ebenezer`.`voluntarios`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_ebenezer`.`voluntarios` (
+  `cod_voluntario` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(60) NOT NULL,
+  `cpf` VARCHAR(11) NOT NULL,
+  `data_nasc` DATETIME NOT NULL,
+  `email` VARCHAR(100) NULL DEFAULT NULL,
+  PRIMARY KEY (`cod_voluntario`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 5
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `db_ebenezer`.`end_voluntario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_ebenezer`.`end_voluntario` (
+  `cod_voluntario` INT(11) NOT NULL,
+  `cod_endereco` INT(11) NOT NULL,
+  PRIMARY KEY (`cod_voluntario`, `cod_endereco`),
+  INDEX `cod_endereco_idx` (`cod_endereco` ASC) VISIBLE,
+  CONSTRAINT `cod_end_vol`
+    FOREIGN KEY (`cod_endereco`)
+    REFERENCES `db_ebenezer`.`endereco` (`cod_endereco`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `cod_voluntario`
+    FOREIGN KEY (`cod_voluntario`)
+    REFERENCES `db_ebenezer`.`voluntarios` (`cod_voluntario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
 -- Table `db_ebenezer`.`nivel`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db_ebenezer`.`nivel` (
@@ -177,6 +171,79 @@ DEFAULT CHARACTER SET = utf8mb4;
 
 
 -- -----------------------------------------------------
+-- Table `db_ebenezer`.`ong`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_ebenezer`.`ong` (
+  `cod_ong` INT(11) NOT NULL AUTO_INCREMENT,
+  `CNPJ` INT(14) NOT NULL,
+  `descricao` LONGTEXT NOT NULL,
+  PRIMARY KEY (`cod_ong`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 12
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `db_ebenezer`.`outro`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_ebenezer`.`outro` (
+  `objeto` VARCHAR(45) NOT NULL,
+  `cod_doacao` INT(11) NOT NULL,
+  INDEX `cod_outro_doacao` (`cod_doacao` ASC) VISIBLE,
+  CONSTRAINT `cod_outro_doacao`
+    FOREIGN KEY (`cod_doacao`)
+    REFERENCES `db_ebenezer`.`doacoes` (`cod_doacoes`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `db_ebenezer`.`tel_filial`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_ebenezer`.`tel_filial` (
+  `cod_telefone` INT(11) NOT NULL,
+  `cod_filial` INT(11) NOT NULL,
+  PRIMARY KEY (`cod_telefone`, `cod_filial`),
+  INDEX `codl_filial_filial` (`cod_filial` ASC) VISIBLE,
+  CONSTRAINT `cod_tel_filial`
+    FOREIGN KEY (`cod_telefone`)
+    REFERENCES `db_ebenezer`.`telefone` (`cod_telefone`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `codl_filial_filial`
+    FOREIGN KEY (`cod_filial`)
+    REFERENCES `db_ebenezer`.`filial` (`cod_filial`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
+-- Table `db_ebenezer`.`tel_voluntario`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_ebenezer`.`tel_voluntario` (
+  `cod_voluntario` INT(11) NOT NULL,
+  `cod_telefone` INT(11) NOT NULL,
+  PRIMARY KEY (`cod_voluntario`, `cod_telefone`),
+  INDEX `cod_tel_voluntario` (`cod_telefone` ASC) VISIBLE,
+  CONSTRAINT `cod_tel_voluntario`
+    FOREIGN KEY (`cod_telefone`)
+    REFERENCES `db_ebenezer`.`telefone` (`cod_telefone`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `cod_vol_voluntario`
+    FOREIGN KEY (`cod_voluntario`)
+    REFERENCES `db_ebenezer`.`voluntarios` (`cod_voluntario`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4;
+
+
+-- -----------------------------------------------------
 -- Table `db_ebenezer`.`usuarios`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db_ebenezer`.`usuarios` (
@@ -185,7 +252,7 @@ CREATE TABLE IF NOT EXISTS `db_ebenezer`.`usuarios` (
   `data_registro` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP(),
   `email` VARCHAR(100) NOT NULL,
   `senha` VARCHAR(32) NOT NULL,
-  `cod_nivel` INT(11) NOT NULL,
+  `cod_nivel` INT(11) NULL DEFAULT NULL,
   PRIMARY KEY (`cod_usuario`),
   UNIQUE INDEX `nome_UNIQUE` (`nome` ASC) VISIBLE,
   UNIQUE INDEX `email_UNIQUE` (`email` ASC) VISIBLE,
